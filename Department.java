f08f6e000cb7666d9739b564bd3fcc7a4821e053
Zach Hall <zachhallnc@gmail.com>
2020-05-20 10:31:01 -0400

    Add abstract Department class

---
+++ Department.java
@@ -0,0 +1,21 @@
+public abstract class Department {
+    private String deptName;
+    private int numEmployees;
+
+    public Department() {
+        this.deptName = "Generic";
+        this.numEmployees = 0;
+    }
+
+    public Department(String name, int numEmployees) {
+        this.deptName = name;
+        this.numEmployees = numEmployees;
+    }
+
+    public abstract boolean isOpenToday(LocalDateTime day);
+
+    @Override
+    public String toString() {
+        return this.deptName + " Department";
+    }
+}
