cae23d5c066148edb11eee49edb6605f0501e928
Zach Hall <zachhallnc@gmail.com>
2020-05-21 08:38:44 -0400

    Create association with Vehicle in ServiceDepartment

--- ServiceDepartment.java
+++ ServiceDepartment.java
@@ -7,6 +7,18 @@ public class ServiceDepartment extends Department {
         super("Service", 20);
     }
 
+    // Create "association" between Vehicle and ServiceDepartment
+    // Very loose, lifetime of objects are independent of one another
+    public boolean changeEngineOil(Vehicle v) {
+        // Because ServiceDepartment doesn't "own" the lifetime of the object its
+        // good practice to ensure a valid object has been passed in
+        if (v != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public boolean isOpenToday(LocalDateTime day) {
         // SUNDAY is an example of an enumeration
